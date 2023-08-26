b46d2c9fe5326f4cdf800b3cd2c0409af31176c7
Adam Berlin <aberlin@vmware.com>
2023-08-26 15:55:13 -0400

    Prevent concurrent setup/teardown
    Authored-by: Adam Berlin <aberlin@vmware.com>

--- testsuite.go
+++ testsuite.go
@@ -1,6 +1,7 @@
 package testsuite
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -36,19 +37,18 @@ type S struct {
 
 	// lock
 	performedSuiteSetup bool
+	mu                  sync.Mutex
 }
 
 // Run performs a *testing.T Run behavior with the setup/teardown behavior of the
 // testsuite
 func (s2 *S) Run(name string, f func(*testing.T)) {
 	// Ensure we only call setup suite and teardown suite once
-	// todo: consider parallelism, race conditions
-	if !s2.performedSuiteSetup {
+	if s2.needsSetup() {
 		s2.setupSuite(s2.t)
 		s2.t.Cleanup(func() {
 			s2.teardownSuite(s2.t)
 		})
-		s2.performedSuiteSetup = true
 	}
 
 	s2.t.Run(name, func(t *testing.T) {
@@ -81,4 +81,16 @@ func (s2 *S) SetupSuite(f func(t *testing.T)) {
 // TeardownSuite specifies behavior that should be run after running all tests in the suite
 func (s2 *S) TeardownSuite(f func(t *testing.T)) {
 	s2.teardownSuite = f
+}
+
+func (s2 *S) needsSetup() bool {
+	// double check that the reader always gets the right value
+	s2.mu.Lock()
+	defer func() {
+		s2.mu.Unlock()
+	}()
+
+	val := s2.performedSuiteSetup
+	s2.performedSuiteSetup = true
+	return !val
 }
